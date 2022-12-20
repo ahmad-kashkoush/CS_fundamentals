fc8be666baa416f529a715b4c3d1f15526c0ed15
ahmed-kashkoush <ahmedkashkoush464@gmail.com>
2022-12-20 22:44:03 +0200

    first knapsack

---
+++ Algorithms2_course/knapsack1.cpp
@@ -0,0 +1,46 @@
+//والله يابني مانا عارف ادينا بنهبد
+//كود بلا هدف, تكبييييير
+// Problem Link -->
+/**symbols
+    @!  --> important and and write it down
+*/
+#include<bits/stdc++.h>
+#define el '\n'
+#define ll long long
+#define reset(v , d) memset(v, d , sizeof(v))
+#define all(s)  s.begin(), s.end()
+#define cin(v)  for(auto &i:v)cin>>i
+#define cout(v) for(auto i:v)cout<<i<<" "
+using namespace std;
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+int dx[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+void solve(){
+
+
+}
+int main() {
+    ios_base::sync_with_stdio(0),
+    cin.tie(0),cout.tie(0);
+    int t=1;
+  //  cin>>t;
+    for(int tc=1;tc<=t;tc++){
+        //cout<<"#tc"<< tc<<": ";
+        solve();
+    //    cout<<endl;
+        //cout<<el;
+    }
+
+    return 0;
+
+
+}
+
+
+
+
+
+
+
+
+
+
